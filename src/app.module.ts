2485c7f96fae83c381c6ea73095a1439f827948e
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-01-14 16:34:37 +0300

    feat: add ServeStatic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config/dist';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './Auth/auth.module';
@@ -9,6 +11,9 @@ import { LetterModule } from './letter/letter.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static/letter'),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://user:useruser@cluster0.zmdfs1v.mongodb.net/?retryWrites=true&w=majority',
     ),
