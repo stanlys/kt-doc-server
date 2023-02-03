c75fe6112f88fec6b81a93eef0be4d241fc0ccbf
Stanlys <stanlys@yandex.ru>
2023-02-03 13:10:29 +0300

    feat: add Delivery endpoint

--- src/deliry-organization/deliry-organization.service.ts
+++ src/deliry-organization/deliry-organization.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateDeliryOrganizationDto } from './dto/create-deliry-organization.dto';
 import { UpdateDeliryOrganizationDto } from './dto/update-deliry-organization.dto';
+import { Delivery, DeliveryDocument } from './schema/deliry.schema';
 
 @Injectable()
 export class DeliryOrganizationService {
-  create(createDeliryOrganizationDto: CreateDeliryOrganizationDto) {
-    return 'This action adds a new deliryOrganization';
+  constructor(
+    @InjectModel(Delivery.name)
+    private readonly DeliveryModel: Model<DeliveryDocument>,
+  ) {}
+
+  async create(createDeliryOrganizationDto: CreateDeliryOrganizationDto) {
+    const postman = await this.DeliveryModel.create(
+      createDeliryOrganizationDto,
+    );
+    return postman;
   }
 
-  findAll() {
-    return `This action returns all deliryOrganization`;
+  async findAll() {
+    const postmans = await this.DeliveryModel.find();
+    return postmans;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} deliryOrganization`;
+  async findOne(id: number) {
+    const postman = await this.DeliveryModel.findById(id);
+    return postman;
   }
 
   update(id: number, updateDeliryOrganizationDto: UpdateDeliryOrganizationDto) {
     return `This action updates a #${id} deliryOrganization`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} deliryOrganization`;
+  async remove(id: number) {
+    const postman = this.DeliveryModel.findByIdAndRemove(id);
+    return postman;
   }
 }
